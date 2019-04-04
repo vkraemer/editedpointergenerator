2b320f3433df2f4dfca0a4f68a8536c4bf6810b3
Erin Kraemer <erinkraemer@erins-mbp.attlocal.net>
2019-04-03 23:28:35 -0700

    change Queue to queue

--- batcher.py
+++ batcher.py
@@ -234,8 +234,8 @@ class Batcher(object):
     self._single_pass = single_pass
 
     # Initialize a queue of Batches waiting to be used, and a queue of Examples waiting to be batched
-    self._batch_queue = Queue.Queue(self.BATCH_QUEUE_MAX)
-    self._example_queue = Queue.Queue(self.BATCH_QUEUE_MAX * self._hps.batch_size)
+    self._batch_queue = queue.queue(self.BATCH_QUEUE_MAX)
+    self._example_queue = queue.queue(self.BATCH_QUEUE_MAX * self._hps.batch_size)
 
     # Different settings depending on whether we're in single_pass mode or not
     if single_pass:
